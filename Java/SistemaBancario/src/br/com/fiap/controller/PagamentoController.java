4b4abbde2d24fe24f798c7f96c458adf40a1aef8
Diego Faria <diego.sb.faria@gmail.com>
2012-11-12 16:46:43 -0200

    Text error customizavel

---
+++ Java/SistemaBancario/src/br/com/fiap/controller/PagamentoController.java
@@ -0,0 +1,110 @@
+package br.com.fiap.controller;
+
+import java.sql.SQLException;
+import java.util.Map;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.component.UIComponent;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.ValidatorException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+import br.com.fiap.dao.PessoaDAO;
+import br.com.fiap.util.Util;
+
+@ManagedBean
+
+public class PagamentoController implements Controller {
+	private int senha;
+	private Double valor;
+	private String codigo;
+	private String saldo;	
+	private Integer data;
+	
+	public String efetuarPagamento(){
+		String retorno = "erro";
+		ExternalContext context = FacesContext.getCurrentInstance()
+				.getExternalContext();
+		HttpServletRequest request = (HttpServletRequest) context.getRequest();
+		
+		if(Util.isSessionValid(request)){
+			PessoaDAO dao = new PessoaDAO();
+			try {
+				if(Double.parseDouble(getSaldo()) >= valor){
+					dao.efetuarPagamento(this);
+					retorno = "home";
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}else{
+			retorno = "login";
+		}
+		return retorno;
+	}
+
+	public void validateSenha(FacesContext context, UIComponent componentToValidate, Object value){
+
+		ExternalContext externalContext = FacesContext.getCurrentInstance()
+				.getExternalContext();
+		Map<String, Object> cookies = externalContext.getSessionMap();
+		PessoaController pessoa = (PessoaController)cookies.get("pessoaController");
+		if(senha != pessoa.getSenha()){
+			FacesMessage message = new FacesMessage("Senhas incompat’veis.");
+			throw new ValidatorException(message);
+		}
+	}
+	
+	public int getSenha() {
+		return senha;
+	}
+
+	public void setSenha(int senha) {
+		this.senha = senha;
+	}
+
+	public Double getValor() {
+		return valor;
+	}
+
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getSaldo() {
+		PessoaDAO dao = new PessoaDAO();
+		ExternalContext context = FacesContext.getCurrentInstance()
+				.getExternalContext();
+		HttpServletRequest request = (HttpServletRequest) context.getRequest();
+		try {
+			saldo = dao.getSaldo(Integer.parseInt(Util.getContaID(request)));
+		} catch (NumberFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return saldo;
+	}
+
+	public Integer getData() {
+		return data;
+	}
+
+	public void setData(Integer data) {
+		this.data = data;
+	}
+	
+}
--- Java/SistemaBancario/src/br/com/fiap/dao/PessoaDAO.java
+++ Java/SistemaBancario/src/br/com/fiap/dao/PessoaDAO.java
@@ -6,47 +6,53 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
+
 import br.com.fiap.controller.Controller;
+import br.com.fiap.controller.PagamentoController;
 import br.com.fiap.controller.PessoaController;
 import br.com.fiap.factory.ConnectionFactory;
 import br.com.fiap.model.Model;
 import br.com.fiap.model.Pessoa;
+import br.com.fiap.util.Util;
 
 public class PessoaDAO implements DAO {
 
 	@Override
 	public void deletar(int id) throws SQLException {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void inserir(Controller controler) throws SQLException {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void atualizar(Controller controler) throws SQLException {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public Model existe(Controller controller) throws SQLException {
 		PessoaController pessoa = (PessoaController) controller;
 		Connection conn = ConnectionFactory.getConnection();
-		
+
 		String sql = "CALL preLogin(?,?,?)";
-		
+
 		PreparedStatement stmt = conn.prepareStatement(sql);
 		stmt.setInt(1, pessoa.getAgencia());
 		stmt.setInt(2, pessoa.getConta());
 		stmt.setInt(3, pessoa.getDigito());
-		
+
 		ResultSet rs = stmt.executeQuery();
 		Pessoa p = null;
-		if(rs.first()){
+		if (rs.first()) {
 			p = new Pessoa();
 			p.setId(rs.getInt("id"));
 			p.setAgencia(pessoa.getAgencia());
@@ -54,54 +60,73 @@ public class PessoaDAO implements DAO {
 			p.setDigito(pessoa.getDigito());
 		}
 		conn.close();
-		
-		return p;		
+
+		return p;
 	}
 
 	public boolean verificaSenha(Pessoa pessoa) throws SQLException {
 		Connection conn = ConnectionFactory.getConnection();
-		
+
 		String sql = "CALL login(?,?)";
-		
+
 		PreparedStatement stmt = conn.prepareStatement(sql);
 		stmt.setInt(1, pessoa.getId());
 		stmt.setInt(2, pessoa.getSenha());
-		
+
 		ResultSet rs = stmt.executeQuery();
 		boolean success = false;
-		if(rs.first()){
+		if (rs.first()) {
 			pessoa.setNome(rs.getString("nome"));
 			pessoa.setSobrenome(rs.getString("sobrenome"));
 			success = true;
 		}
 		conn.close();
-		
-		return success;		
+
+		return success;
 	}
-	
-	public String getSaldo(int id) throws SQLException{
+
+	public String getSaldo(int id) throws SQLException {
 		String retorno = null;
-Connection conn = ConnectionFactory.getConnection();
-		
+		Connection conn = ConnectionFactory.getConnection();
+
 		String sql = "CALL saldo(?)";
-		
+
 		PreparedStatement stmt = conn.prepareStatement(sql);
 		stmt.setInt(1, id);
-		
+
 		ResultSet rs = stmt.executeQuery();
-		if(rs.first()){
+		if (rs.first()) {
 			retorno = String.valueOf(rs.getDouble("saldo"));
 		}
-		
+
 		conn.close();
 		return retorno;
 	}
-	
-	@Override
-	public ArrayList<Model> consultar(Controller controler) throws SQLException {
-		return null;		
+
+	public void efetuarPagamento(PagamentoController controller)
+			throws SQLException {
+
+		ExternalContext context = FacesContext.getCurrentInstance()
+				.getExternalContext();
+		HttpServletRequest request = (HttpServletRequest) context.getRequest();
+		Connection conn = ConnectionFactory.getConnection();
+
+		String sql = "CALL pagamento(?,?, ?, ?)";
+
+		PreparedStatement stmt = conn.prepareStatement(sql);
+		stmt.setInt(1, Integer.parseInt(Util.getContaID(request)));
+		stmt.setDouble(2, controller.getValor());
+		stmt.setString(3, controller.getCodigo());
+		stmt.setString(4, String.valueOf(controller.getData()));
+
+		stmt.executeQuery();
+		
+		conn.close();
 	}
 
-
+	@Override
+	public ArrayList<Model> consultar(Controller controler) throws SQLException {
+		return null;
+	}
 
 }
