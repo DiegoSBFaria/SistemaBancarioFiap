d623a3f86d336cd0fc065284e093b4178b667053
Diego Faria <diego.sb.faria@gmail.com>
2012-11-12 12:47:32 -0200

    Cadastro Terceiro

--- Java/SistemaBancario/src/br/com/fiap/controller/TerceiroController.java
+++ Java/SistemaBancario/src/br/com/fiap/controller/TerceiroController.java
@@ -4,9 +4,13 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
 
 import br.com.fiap.dao.TerceiraDao;
 import br.com.fiap.model.Model;
+import br.com.fiap.util.Util;
 
 @ManagedBean
 public class TerceiroController implements Controller {
@@ -18,6 +22,27 @@ public class TerceiroController implements Controller {
 
 	private ArrayList<Model> contas;
 
+	public String cadastrar(){
+		String retorno = "erro";
+		ExternalContext context = FacesContext.getCurrentInstance()
+				.getExternalContext();
+		HttpServletRequest request = (HttpServletRequest) context.getRequest();
+		
+		if(Util.isSessionValid(request)){
+			TerceiraDao dao = new TerceiraDao();
+			try {
+				dao.inserir(this);
+				retorno = "contaTerceiro";
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}else{
+			retorno = "login";
+		}
+		return retorno;
+	}
+	
 	public ArrayList<Model> getContas() {
 		TerceiraDao dao = new TerceiraDao();
 		try {
--- Java/SistemaBancario/src/br/com/fiap/dao/TerceiraDao.java
+++ Java/SistemaBancario/src/br/com/fiap/dao/TerceiraDao.java
@@ -1,11 +1,22 @@
 package br.com.fiap.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Types;
 import java.util.ArrayList;
 
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
+
 import br.com.fiap.controller.Controller;
+import br.com.fiap.controller.TerceiroController;
+import br.com.fiap.factory.ConnectionFactory;
 import br.com.fiap.model.ContaTerceiro;
 import br.com.fiap.model.Model;
+import br.com.fiap.util.Util;
 
 public class TerceiraDao implements DAO {
 
@@ -17,8 +28,38 @@ public class TerceiraDao implements DAO {
 
 	@Override
 	public void inserir(Controller controler) throws SQLException {
-		// TODO Auto-generated method stub
+		TerceiroController terceiro = (TerceiroController) controler;
+		Connection conn = ConnectionFactory.getConnection();
+
+		String id = null;
+		String sql = "SELECT id FROM contas WHERE agencia = ? AND numero = ? AND digito = ? AND tipo_conta = 'c'";
+		PreparedStatement stmt = conn.prepareStatement(sql);
+
+		stmt.setString(1, String.valueOf(terceiro.getAgencia()));
+		stmt.setString(2, String.valueOf(terceiro.getConta()));
+		stmt.setString(3, String.valueOf(terceiro.getDigito()));
+
+		ResultSet rs = stmt.executeQuery();
+		if(rs.first()){
+			id = String.valueOf(rs.getInt(1));
+		}
+		sql = "CALL inserirTerceiro(?,?,?,?,?,?)";
+
+		stmt = conn.prepareStatement(sql);
+		ExternalContext context = FacesContext.getCurrentInstance()
+				.getExternalContext();
+		HttpServletRequest request = (HttpServletRequest) context.getRequest();
+		stmt.setString(1, terceiro.getNome());
+		stmt.setString(2, String.valueOf(terceiro.getAgencia()));
+		stmt.setString(3, String.valueOf(terceiro.getConta()));
+		stmt.setString(4, String.valueOf(terceiro.getDigito()));
+		stmt.setInt(5, Integer.parseInt(Util.getContaID(request)));
+		if(id != null)
+			stmt.setInt(6, Integer.parseInt(id));
+		else
+			stmt.setNull(6, Types.INTEGER);
 		
+		stmt.executeQuery();
 	}
 
 	@Override
